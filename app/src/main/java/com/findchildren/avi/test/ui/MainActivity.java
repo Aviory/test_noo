c52f83c06df63971b25fa18512817802f5f2f472
avi <avior557@gmail.com>
2017-09-29 22:14:53 +0300

    save

--- app/src/main/java/com/findchildren/avi/test/ui/MainActivity.java
+++ app/src/main/java/com/findchildren/avi/test/ui/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.RelativeLayout;
 
 import com.findchildren.avi.test.Const;
@@ -27,8 +31,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mActionBarToolbar);
+
         cardsFragment = new RecycleCardsFragment();
         addFrag(cardsFragment, Const.FRAGMENT_MAIN_TAG);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void addFrag(Fragment fragment, String tag) {
